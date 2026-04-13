ccf364cf0b0cdd9e9eb39575e75e96300c62931d
chikontaiii <cyntemirkurmanbekov53@gmail.com>
2026-04-13 23:39:59 +0600

    аа

--- firebase.js
+++ firebase.js
@@ -4,12 +4,13 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-s
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDNrR5Mxd9xG3C5354EF96r7CKWZme9FXM",
-    authDomain: "pks-7-24-portal.firebaseapp.com",
-    projectId: "pks-7-24-portal",
-    storageBucket: "pks-7-24-portal.firebasestorage.app",
-    messagingSenderId: "657797301237",
-    appId: "1:657797301237:web:9be5433f4444b3896d56bc"
+    apiKey: "AIzaSyAAkX1LEW6JywRHn-vp10SZhjeFa9RbcQk",
+    authDomain: "sssk-1-24.firebaseapp.com",
+    projectId: "sssk-1-24",
+    storageBucket: "sssk-1-24.firebasestorage.app",
+    messagingSenderId: "392904114221",
+    appId: "1:392904114221:web:de75a5c2903da52affb8a4",
+    measurementId: "G-WGCCCS47S8"
 };
 
 const app = initializeApp(firebaseConfig);
